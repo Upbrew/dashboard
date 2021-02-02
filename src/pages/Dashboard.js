0c100ff78a860f16ac432c075088641ea616e133
Bibin Mathew <bibin765@gmail.com>
2021-02-02 09:13:28 +0530

    added dependencies

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Dashboard() {
+    return (
+        <div className="dashboard">
+            {/* Explorer */}
+            {/* DetailsPane */}
+        </div>
+    )
+}
+
+export default Dashboard
